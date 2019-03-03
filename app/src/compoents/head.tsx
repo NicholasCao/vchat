e7312ca762ecec3e355da6044324ff3d535ab925
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-03 12:15:15 +0800

    feat: updaye head

---
+++ app/src/compoents/head.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Image, TouchableHighlight, StyleSheet, Text, View } from "react-native"
+// import { withNavigation } from 'react-navigation';
+
+interface Props {
+
+}
+export default class ChatBox extends React.Component<any,any> {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+    };
+  }
+  render():React.ReactNode {
+    return (
+      <View></View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  
+})
