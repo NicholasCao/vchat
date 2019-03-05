56700552b0bf178deffcc3f755657fbcf9f4690b
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-05 22:59:33 +0800

    feat: use ws

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,17 +12,36 @@ ctx.body = 'Hello World'
 const server = http.createServer(app.callback())
 const wss = new WebSocket.Server({server})
 
-wss.on('connection', (ws) => {
+var userList:any[] = []
+wss.on('connection', (ws,req) => {
 	console.log(`[SERVER] connection()`)
-	ws.on('open', function open() {
-		ws.send('something')
-	})
-	ws.on('message', function (message) {
-		console.log(`[SERVER] Received: ${message}`)
-		ws.send(`Server: ${message}`, (err) => {
-			if (err) {
-				console.log(`[SERVER] error: ${err}`)
-			}
+	// ws.on('open', function open() {
+	// 	ws.send('something')
+	// })
+	ws.on('message', (msg:string) => {
+		const data = JSON.parse(msg)
+		//console.log(data)
+
+		// 加入userList
+		if(userList.indexOf(data.from) === -1){
+			userList.push(data.from)
+		}
+		let fromIndex = userList.indexOf(data.from)
+		// console.log(userList)
+		let index = userList.indexOf(data.to)
+
+		// 用户离线
+		if (index === -1) return
+
+		// 发送消息到对应的客户端
+		Array.from(wss.clients)[index].send(JSON.stringify(data), (err) => {
+			if (err) console.log(`[SERVER] error: ${err}`)
+		})
+
+		// 断开连接时删除对应的user
+		ws.on('close',() => {
+			userList.splice(fromIndex,1)
+			//console.log(userList)
 		})
 	})
 })
