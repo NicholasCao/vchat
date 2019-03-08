3e846808db5563f141c0fe23e69544132a5d85a3
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-08 22:03:25 +0800

    feat: use koa-bodyParser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,25 @@
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
+import * as bodyParser from 'koa-bodyparser'
+import * as json from 'koa-json'
 import * as http from 'http'
 import * as WebSocket from 'ws'
 import * as url from 'url'
+import * as mongoose from 'mongoose'
 
+import config from './config'
 import router from './routes'
 import msgWs from './ws/msgWs'
 import userWs from './ws/userWs'
 
+mongoose.connect(config.db, { useNewUrlParser: true })
+mongoose.connection.on("error", console.error)
+mongoose.connection.on("open", () => console.log("MongoDB connection successed"))
+
 const app = new Koa()
 
+app.use(bodyParser())
+app.use(json())
 app.use(router.routes())
 
 const server = http.createServer(app.callback())
@@ -30,7 +40,7 @@ server.on('upgrade', (request, socket, head) => {
     msgWs.handleUpgrade(request, socket, head, (ws) => {
       msgWs.emit('connection', ws)
     })
-  } else if (pathname === '/bar') {
+  } else if (pathname === '/user') {
     userWs.handleUpgrade(request, socket, head, (ws) => {
       userWs.emit('connection', ws)
     })
