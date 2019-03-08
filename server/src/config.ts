145cee35d1a8ae4dd1544219fdd8caadacc7a0f1
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-08 22:18:11 +0800

    feat: update config

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 
 export default {
-  db: 'mongodb://localhost:27017/test2',
+  db: 'mongodb://localhost:27017/vchattest',
   port: '3002',
   // root: '',
   secret: 'test'
