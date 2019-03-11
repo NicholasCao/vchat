c2cfa45ec52da77a6020925c8c94573382134d45
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-03-11 21:22:20 +0800

    feat: add littleV

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -3,21 +3,64 @@ import * as jwt from 'jsonwebtoken'
 import User from '../models/user'
 import config from '../config'
 
+interface LittleV {
+  user: any,
+  init: any,
+  addFriend: any
+}
+// 注册小V
+const littleV:LittleV = {
+  user: null,
+  init: async function () {
+    let username = 'littleV',
+      password = config.littleVPassword,
+      name = '小V'
+    var user:any = await User.findOne({ username }).catch(err => {
+      console.log(err)
+    })
+    if (user == null) {
+      user = new User({
+        username,
+        password,
+        name,
+        contacts: []
+      })
+      user = await user.save().catch((err:any) => {
+        console.log(err)
+      })
+    }
+    this.user = user
+    console.log(user)
+  },
+  addFriend: async function (user:any) {
+    this.user.contacts.push(user.username)
+    this.user = this.user.save().catch((err:any) => {
+      console.log(err)
+    })
+  }
+}
+
+littleV.init()
 
 // 登录
 const login = async (ctx:any) => {
   const { username, password } = ctx.request.body
   if(new RegExp(/^[A-Za-z0-9]{6,10}$/).test(username) && new RegExp(/^[A-Za-z0-9]{6,10}$/).test(password)){
-    let user:any = await User.findOne({ username })
+    let user:any = await User.findOne({ username }, ['name', 'username', '_id'])
+    .catch(err => {
+      console.log(err)
+    })
     if(user != null) {
       if(password == user.password) {
         const token = jwt.sign({
           _id: user._id,
         }, config.secret)
+        let contacts = await User.find({'username': {'$in': user.contacts}})
         ctx.body = {
           success: true,
           user,
-          token
+          token,
+          contacts
         }
       } else {
         // 密码不正确
@@ -44,7 +87,6 @@ const login = async (ctx:any) => {
 
 // 注册
 const signUp = async (ctx:any) => {
-  console.log(ctx.request.body)
   const { username, password, name } = ctx.request.body
   // 检测名字
   if(!new RegExp(/^[\S|\d]{0,10}$/).test(name)){
@@ -65,7 +107,8 @@ const signUp = async (ctx:any) => {
       info: '密码应为6-12位英文字符或数字'
     }
   } else {
-    let user:any = await User.findOne({ username }).catch(err => {
+    let user:any = await User.findOne({ username }, ['name', 'username', '_id', 'contacts'])
+    .catch(err => {
       console.log(err)
     })
     if(user != null) {
@@ -74,16 +117,16 @@ const signUp = async (ctx:any) => {
         info: 'username already exists'
       }
     } else {
-      var a = {b:1}
       user = new User({
         username,
         password,
         name,
-        contacts: new Map([['a',a]])
+        contacts: [littleV.user.username]
       })
       let result = await user.save().catch((err:any) => {
         console.log(err)
       })
+      littleV.addFriend(result)
       ctx.body = {
         success: true,
         user: result
@@ -92,27 +135,47 @@ const signUp = async (ctx:any) => {
   }
 }
 
-// 添加好友
+
+/*
+/ 添加好友(已同意)
+/ @param _id 同意的人ID
+/ @param friendUsername 申请人的用户名
+/ @return  
+*/
 const addFriend = async (ctx:any) => {
-  const { _id, friendId, friendUsername } = ctx.request.body
-  let user:any = await User.findOne({ _id }).catch(err => {
+  const  { _id, friendUsername } = ctx.request.body
+  let user:any = await User.findOne({ _id }, ['name', 'username', '_id', 'contacts']).catch(err => {
     console.log(err)
   })
-  if(friendId){
-    let friend:any = await User.findOne({ friendId }).catch(err => {
-      console.log(err)
-    })
-  } else if(friendUsername) {
-    let friend:any = await User.findOne({ friendUsername }).catch(err => {
-      console.log(err)
-    })
+  let friend:any = await User.findOne({ friendUsername }, ['name', 'username', '_id', 'contacts']).catch(err => {
+    console.log(err)
+  })
+  user.contacts = user.contacts.push(friend)
+  friend.contacts = user.contacts.push(user)
+  try{
+    let result1 = await user.save()
+    let result2 = await friend.save()
+    // 将申请人的用户信息返回
+    ctx.body = {
+      success: true,
+      friend: result2
+    }
+    // ws通知申请人并发送接受者的用户信息
+    // result1
+  }
+  catch(err) {
+    console.log(err)
+    ctx.body = {
+      success: false,
+      info: err
+    }
   }
 }
 
 // 搜索用户
 const search = async (ctx:any) => {
-  const username  = ctx.params.username
-  let user:any = await User.findOne({ username }, ['name', 'username', '_id'])
+  const { username } = ctx.params
+  let user:any = await User.findOne({ username }, ['name', 'username', '_id', 'contacts'])
   .catch(err => {
     console.log(err)
   })
